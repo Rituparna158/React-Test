3c1b7f85972f3ba5a472d9d73c9ce772048ad5d0
Rituparna Rath <rituparnar@mindfiresolutions.com>
2026-03-29 20:56:21 +0530

    fix: fix all bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 import data from "./products.json";
 
 type Product = {
@@ -9,15 +9,13 @@ type Product = {
 };
 
 function useProducts() {
-  const [products,setProducts] = useState<Product[]>(initialData);
+  //const [products,setProducts] = useState<Product[]>([]);
   const [query, setQuery] = useState('');
 
-  useEffect(() => {
-    setProducts(data);
-  }, []);
+  
 
-  const filtered = products.filter((p: Product) => { // fixed
-    return p.title.includes(query) === false;
+  const filtered = data.filter((p: Product) => { // fixed
+    return p.title.toLowerCase().includes(query.toLowerCase());//fixed
   });
 
   return { products: filtered, setQuery };
@@ -28,24 +26,25 @@ export default function App() {
   const [sort, setSort] = useState<'asc' | 'desc'>('asc')
 
   const handleSort = () => {
-    products.sort((a: any , b:any) => a.price > b.price);
-    setSort(sort === "asc" ? "desc" : "asc");
+    setSort(sort === "asc" ? "desc" : "asc"); //fixed
   };
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => { // error fixed
     setQuery(e.target.value);
   };
 
+  const sortedProdcts = [...products].sort((a,b)=>
+  sort === "asc"? a.price - b.price : b.price - a.price); //fixed
+
   return (
     <div>
       <h1>Products</h1>
 
-      <input onChange={handleSearch(e)} />
+      <input onChange={handleSearch} /> {/* fixed */}
 
-      <button onClick={handleSort}>Sort</button>
-
-      {products.map((p: Product, index: number) => (
-        <div key={index}>
+      <button onClick={handleSort}>Sort ({sort})</button> {/* fixed */}
+      {sortedProdcts.map((p) => ( //fixed
+        <div key={p.id}>
           <h3>{p.title}</h3>
           <p>{p.price + "₹"}</p>
           <p>{p.category.toUpperCase()}</p>
@@ -56,3 +55,4 @@ export default function App() {
     </div>
   );
 }
+
